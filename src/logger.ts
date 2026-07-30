30062116b886067a321bda182a84bb3d5d97407f
mybox1407-art <mybox1407@gmail.com>
2026-07-30 14:22:32 +0400

    Create logger.ts

---
+++ src/logger.ts
@@ -0,0 +1,16 @@
+import pino from "pino";
+import { config } from "./config.js";
+
+export const logger = pino({
+  level: config.logLevel,
+  transport:
+    config.nodeEnv === "development"
+      ? {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: "SYS:standard"
+          }
+        }
+      : undefined
+});
